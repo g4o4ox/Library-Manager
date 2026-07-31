e9443f964891150284056bb26f54add841c9461c
GROX <103297099+g4o4ox@users.noreply.github.com>
2026-07-31 10:06:00 -0300

    Add Book, Operator, and Client structures
    Define structures for Book, Operator, and Client. Implement main function with a loop.

---
+++ manager.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+struct Book {
+	char name[75];
+	char type[30];
+	int     bookcode;	
+};
+struct Operator{
+	char name[80];
+	char cargo[50];
+	char password[9];
+	int code;
+
+};
+struct Client{
+	char name[80];
+	char cpf[20];
+};
+
+int main(){
+	
+	int a = 0;
+	while( a < 1){
+		
+		int exit;
+		if(exit == 4){
+			a++;
+		}
+
+
+}
+	
+
+
+	
